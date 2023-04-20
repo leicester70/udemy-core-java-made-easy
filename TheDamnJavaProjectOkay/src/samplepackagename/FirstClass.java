387bfbe47e90ff6c729ccafcde573af684b73617
C. L <82318965+leicester70@users.noreply.github.com>
2023-04-20 19:06:54 +0800

    shortcuts and print thing sysout whater

--- TheDamnJavaProjectOkay/src/samplepackagename/FirstClass.java
+++ TheDamnJavaProjectOkay/src/samplepackagename/FirstClass.java
@@ -1,6 +1,8 @@
 package samplepackagename;
 
 public class FirstClass {
-    public static void main(String[] args) {
-    }
+	public static void main(String[] args) {
+		System.out.println("WOW I can h" + "it ctrl+spacebar for help! Eclipse is not so lame after all");
+		// I can format crap here by ctrl+shift+F
+	}
 }
