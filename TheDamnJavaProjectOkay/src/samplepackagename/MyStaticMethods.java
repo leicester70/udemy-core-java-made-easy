57f842ceaa3e7d2a51b1c8d42a98b68ec8151929
C. L <82318965+leicester70@users.noreply.github.com>
2023-04-22 16:47:53 +0800

    change previous commit

--- TheDamnJavaProjectOkay/src/samplepackagename/MyStaticMethods.java
+++ TheDamnJavaProjectOkay/src/samplepackagename/MyStaticMethods.java
@@ -17,7 +17,6 @@ public class MyStaticMethods {
 		System.out.println("A: \rThe main difference between a static block and a static method is that a static block is executed only once when the class is loaded into memory, while a static method can be called multiple times throughout the lifetime of a program. Both static blocks and static methods can access and modify static variables, but static methods cannot access non-static variables or methods directly.\r\n"
 				+ "\r\n"
 				+ "A static block is useful for initializing static variables, especially when the initialization requires complex logic or exception handling. Static blocks are executed in the order they appear in the code, and they can be used for one-time setup tasks, such as loading configuration data or initializing resources. However, static blocks cannot be called directly, and they are executed automatically when the class is loaded. Here is an example of a static block in Java:");
-		System.out.println("[executing myFirstMethod() in a static block]");
 		MyStaticMethods.myFirstMethod("tag2");
 	}
 
