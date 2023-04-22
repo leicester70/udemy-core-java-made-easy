4102d3ee9ea22926d8f89ab32153d836100c407e
C. L <82318965+leicester70@users.noreply.github.com>
2023-04-22 16:18:58 +0800

    now i invoke(not like i dont know this)

--- TheDamnJavaProjectOkay/src/samplepackagename/MyStaticMethods.java
+++ TheDamnJavaProjectOkay/src/samplepackagename/MyStaticMethods.java
@@ -3,11 +3,11 @@ package samplepackagename;
 public class MyStaticMethods {
 
 	public static void main(String[] args) {
-		
+		MyStaticMethods.myFirstMethod();
 	}
-	
+
 	static void myFirstMethod() {
-		System.out.println("Inside method my first method.");
+		System.out.println("Inside method my first method. It has to be invoked. See line 5");
 	}
 
 }
