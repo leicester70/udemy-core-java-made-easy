293cd88e32ce87238f07279850fdbdc993cae2bc
C. L <82318965+leicester70@users.noreply.github.com>
2023-04-22 16:11:45 +0800

    static blocks i guess

--- TheDamnJavaProjectOkay/src/samplepackagename/FirstClass.java
+++ TheDamnJavaProjectOkay/src/samplepackagename/FirstClass.java
@@ -2,7 +2,14 @@ package samplepackagename;
 
 public class FirstClass {
 	public static void main(String[] args) {
-		System.out.println("WOW I can h" + "it ctrl+spacebar for help! Eclipse is not so lame after all");
-		// I can format crap here by ctrl+shift+F
+		System.out.println("WOW I can hit" + "ctrl+spacebar" + " for help! Eclipse is not so lame after all");
+	}
+
+	static {
+		System.out.println("Static Block 1:  it will execute first");
+	}
+
+	static {
+		System.out.println("Static block 2: This should come out second I think? I declared it the second block. [Edit 2] I was right -  of course what the fuck, it is top down isn't it? (JVM runs in order)");
 	}
 }
