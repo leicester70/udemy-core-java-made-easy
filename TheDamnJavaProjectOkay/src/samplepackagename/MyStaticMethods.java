bb36e050aa6f7c1981fa047b65e3f4c15854b80a
C. L <82318965+leicester70@users.noreply.github.com>
2023-04-22 16:12:20 +0800

    created my first static method

---
+++ TheDamnJavaProjectOkay/src/samplepackagename/MyStaticMethods.java
@@ -0,0 +1,13 @@
+package samplepackagename;
+
+public class MyStaticMethods {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	static void myFirstMethod() {
+		System.out.println("Inside method my first method.");
+	}
+
+}
