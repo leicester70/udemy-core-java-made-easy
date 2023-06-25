a7cefe193edf5126b9c0b5848d854044161cabd0
C <leicester70@users.noreply.github.com>
2023-06-25 17:59:36 +0800

    stuff

--- TheDamnJavaProjectOkay/src/myminiapp/CustomController.java
+++ TheDamnJavaProjectOkay/src/myminiapp/CustomController.java
@@ -1,22 +1,39 @@
 package myminiapp;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CustomController {
-    // Methods
+
     public boolean ValidateCredentials(String accessCode, String sixDigitPin) {
         String regex = "\\b\\d{6}\\b";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(sixDigitPin);
         if (matcher.find()) {
-            System.out.println("Found 6 Digit PIN \nEND");
+            System.out.println("Found 6 Digit PIN");
+        } else {
+            System.out.println("Invalid PIN. PIN is not 6 digits");
+        }
+        if (isFoundCrendentials(accessCode, sixDigitPin)) {
+            return true;
         }
         return false;
     }
 
-    public boolean isFoundCrendentials(String accessCode, String sizDigitString) {
-        System.out.println();
+    public boolean isFoundCrendentials(String accessCode, String sixDigitPin) {
+        ArrayList<Pair<String, String>> accountsArrList = new ArrayList<>();
+        accountsArrList.add(new Pair<String, String>("lester123", "123456"));
+        accountsArrList.add(new Pair<String, String>("321gwin", "654321"));
+
+        Pair passed = new Pair<String, String>(accessCode, sixDigitPin);
+        for (Pair<String, String> pair : accountsArrList) {
+            if (pair.getKey().equals(passed.getKey()) && pair.getValue().equals(passed.getValue())) {
+                System.out.println("Valid User");
+                return true;
+            }
+        }
+        System.out.println("Invalid User");
         return false;
     }
 }
--- TheDamnJavaProjectOkay/src/myminiapp/MicroATM.java
+++ TheDamnJavaProjectOkay/src/myminiapp/MicroATM.java
@@ -1,19 +1,33 @@
 package myminiapp;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MicroATM {
-    static {
-        ArrayList<Pair<String, String>> accountsArrList = new ArrayList<>();
-        accountsArrList.add(new Pair<String, String>("lester123", "123456"));
-    }
 
     public static void main(String[] args) {
-        Pair<Boolean, String> isArgsFound = (args.length > 0) ? new Pair<>(true, "args populated")
-                : new Pair<>(false, "args NULL");
-        System.out.println(isArgsFound.getValue());
         CustomController controller = new CustomController();
-        System.out.println(controller.ValidateCredentials("lestercheong70", "123456"));
+        String accessCode = "", pin = "";
+        boolean validUser = false;
+        try {
+            Pair<Boolean, String> isArgsFound = (args.length > 0) ? new Pair<>(true, "args populated")
+                    : new Pair<>(false, "args NULL");
+            System.out.println(isArgsFound.getValue());
+            accessCode = args[0];
+            pin = args[1];
+
+        } catch (Exception e) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(
+                    "enter access code and pin, delimited by comma ','\nexample: accesscode123,123456\nenter:\t");
+            String[] input = scanner.nextLine().split(",");
+            try {
+                accessCode = input[0];
+                pin = input[1];
+            } catch (Exception e1) {
+                System.out.println("did you deliminate properly?\n[ERROR]\t\n" + e + "\t\n" + e1);
+            }
+        }
+        validUser = ValidateCredentials(accessCode, pin);
     }
 
 }
